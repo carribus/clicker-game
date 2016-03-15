0966f5ced77bafad00aaca85f02ccc82c6068c06
Peter Mares <peter@odobo.com>
2016-03-15 09:52:19 +0100

    Fixed the bug in auto-click expiry

--- client/src/js/powerups/autoclick.js
+++ client/src/js/powerups/autoclick.js
@@ -4,8 +4,6 @@
 
 module.exports = AutoClickPowerup;
 
-// TODO: Implement time based expiry on auto clicking
-
 function AutoClickPowerup(config) {
     this.clicks_per_second = config.clicks_per_second;
     this.expiresAfter = config.expiresAfter;
@@ -15,7 +13,7 @@ function AutoClickPowerup(config) {
     this.expiryTimestamp;
 
     if ( this.expiresAfter ) {
-        this.expireTimestamp = Date.now() + this.expiresAfter;
+        this.expiryTimestamp = Date.now() + this.expiresAfter;
     }
 
     this.onTick = function(clickEngine) {
