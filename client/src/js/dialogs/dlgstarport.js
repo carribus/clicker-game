d3dc95b42433fdf3a68cd59ff3d2173d16d3f0db
Peter Mares <peter@odobo.com>
2016-03-29 14:39:50 +0200

    Refactored the TradeItemWidget into its own module

--- client/src/js/dialogs/dlgstarport.js
+++ client/src/js/dialogs/dlgstarport.js
@@ -6,6 +6,7 @@ module.exports = DlgStarPort;
 
 var Dialog = require('../objects/dialog');
 var UIButton = require('../objects/uibutton');
+var TradeItemWidget = require('../objects/tradeitemwidget');
 
 var TRADE_ITEMS = [
     {
@@ -132,44 +133,3 @@ DlgStarPort.prototype._createTradePanel = function(game, x, y, width, height) {
     backButton.anchor.set(0.5);
     tradePanel.addChild(backButton);
 };
-
-function TradeItemWidget(game, x, y, width, height, item) {
-    UIButton.call(this, game, x, y, width, height, item.name);
-
-    var fontSize = height * 0.1;
-    this.anchor.set(0.5);
-    this.label.fontWeight = 'bold';
-    this.label.boundsAlignH = 'left';
-    this.label.fontSize = fontSize + 'pt';
-    this.label.y -= 60;
-
-    var fontSize = height * 0.1;
-    this.txtQuantity = game.add.text(0, 0, 'Qty: ' + item.quantity, {
-        font: fontSize + 'pt Arial',
-        boundsAlignH: 'left',
-        boundsAlignV: 'middle',
-        fill: 'white'
-    });
-    this.txtQuantity.anchor.set(0.5);
-    this.addChild(this.txtQuantity);
-
-    this.sellPrice = game.add.text(0, 40, 'Sell: ' + item.sellPrice, {
-        font: fontSize + 'pt Arial',
-        boundsAlignH: 'left',
-        boundsAlignV: 'middle',
-        fill: 'green'
-    });
-    this.sellPrice.anchor.set(0.5);
-    this.addChild(this.sellPrice);
-
-    this.buyPrice = game.add.text(0, 70, 'Buy: ' + item.buyPrice, {
-        font: fontSize + 'pt Arial',
-        boundsAlignH: 'left',
-        boundsAlignV: 'middle',
-        fill: 'red'
-    });
-    this.buyPrice.anchor.set(0.5);
-    this.addChild(this.buyPrice);
-}
-TradeItemWidget.prototype = Object.create(UIButton.prototype);
-TradeItemWidget.prototype.constructor = TradeItemWidget;
